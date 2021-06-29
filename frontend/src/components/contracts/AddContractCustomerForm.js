46e67a7a6b43c913d7d19507b57676dfa76605f0
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2021-06-28 23:06:11 -0700

    Cart Price and Contracts page fixed

--- frontend/src/components/contracts/AddContractCustomerForm.js
+++ frontend/src/components/contracts/AddContractCustomerForm.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Form, Table, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { CartContext } from '../../screens/CartPage/cart'
-import ContractItem from './ContractItem'
-import InputDropdown from '../general/form/InputDropdown'
-import AddContractCustomer from '../../screens/contracts/AddContractScreen'
+import React, { useState, useEffect } from "react";
+import { Form, Table, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../screens/CartPage/cart";
+import ContractItem from "./ContractItem";
+import InputDropdown from "../general/form/InputDropdown";
+import AddContractCustomer from "../../screens/contracts/AddContractScreen";
 
 function AddContractCustomerForm({ match }) {
-  const { cart, subTotal } = React.useContext(CartContext)
-  const [loading, setLoading] = useState(true)
-  const [contract, setContract] = useState({})
-  const bankNames = ['Xalq', 'Turon', 'Agro', 'Milliy', 'Zamin Tech']
-  var currencyFormatter = require('currency-formatter')
+  const { cart, subTotal } = React.useContext(CartContext);
+  const [loading, setLoading] = useState(true);
+  const [contract, setContract] = useState({});
+  const bankNames = ["Xalq", "Turon", "Agro", "Milliy", "Zamin Tech"];
+  var currencyFormatter = require("currency-formatter");
 
   const [data, setData] = useState({
-    bank: '',
-  })
+    bank: "",
+  });
 
   function inputHandler(e) {
-    const newData = { ...data }
-    newData[e.target.id] = e.target.value
-    setData(newData)
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
   }
   useEffect(() => {
     async function fetchMyApi() {
-      const url = 'http://localhost:3001/contracts'
-      const res = await fetch(url)
-      const data = await res.json()
+      const url = "http://localhost:3001/contracts";
+      const res = await fetch(url);
+      const data = await res.json();
       setContract(
         data.find(function (p) {
-          return p.id === match.params.id
+          return p.id === match.params.id;
         })
-      )
+      );
       // setLoading(false);
     }
-    fetchMyApi()
+    fetchMyApi();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   function formatNumber(number) {
-    number = Math.round(number)
-    return currencyFormatter.format(number, { code: '' })
+    number = Math.round(number);
+    return currencyFormatter.format(number, { code: "" });
   }
 
   return (
@@ -48,7 +48,7 @@ function AddContractCustomerForm({ match }) {
       <div>
         <AddContractCustomer />
       </div>
-      <div className='customer-form'>
+      <div className="customer-form mt-5">
         <Form>
           <Form.Label>
             <h2>Xarid Qilingan Mahsulotlar</h2>
@@ -63,39 +63,39 @@ function AddContractCustomerForm({ match }) {
                   <Col xs={6}>
                     <Form.Label>
                       <b>MAHSULOT</b>
-                    </Form.Label>{' '}
+                    </Form.Label>{" "}
                     <br />
                   </Col>
-                  <Col style={{ textAlign: 'center' }}>
+                  <Col>
                     <Form.Label>
                       <b>MIQDORI</b>
-                    </Form.Label>{' '}
+                    </Form.Label>{" "}
                     <br />
                   </Col>
                   <Col>
                     <Form.Label>
                       <b>NARXI</b>
-                    </Form.Label>{' '}
+                    </Form.Label>{" "}
                     <br />
                   </Col>
                   <Col>
                     <Form.Label>
                       <b>UMUMIY SUMMASI</b>
-                    </Form.Label>{' '}
+                    </Form.Label>{" "}
                     <br />
                   </Col>
                 </Form.Row>
                 <Form.Row>
                   <Col>
                     {cart.map((item) => {
-                      return <ContractItem key={item.id} {...item} />
+                      return <ContractItem key={item.id} {...item} />;
                     })}
                   </Col>
                 </Form.Row>
                 <Form.Row>
-                  <Col xs={6}>
-                    <Link to='/products'>
-                      <button type='button' className='btn btn-primary'>
+                  <Col xs={3}>
+                    <Link to="/products">
+                      <button type="button" className="btn btn-primary">
                         Mahsulot Qo'shish
                       </button>
                     </Link>
@@ -107,14 +107,14 @@ function AddContractCustomerForm({ match }) {
             <Form.Row>
               <Col>
                 <Table
-                  size='sm'
+                  size="sm"
                   style={{
-                    fontSize: '1rem',
-                    width: '40%',
-                    marginLeft: 'auto',
-                    textAlign: 'left',
-                    marginRight: '0px',
-                    alignItems: 'center',
+                    fontSize: "1rem",
+                    width: "40%",
+                    marginLeft: "auto",
+                    textAlign: "left",
+                    marginRight: "0px",
+                    alignItems: "center",
                   }}
                 >
                   <thead>
@@ -123,7 +123,7 @@ function AddContractCustomerForm({ match }) {
                       <th>
                         <h6>SUBTOTAL</h6>
                       </th>
-                      <th style={{ textAlign: 'end' }}>
+                      <th style={{ textAlign: "end" }}>
                         {`${formatNumber(subTotal)} сўм`}
                       </th>
                     </tr>
@@ -134,18 +134,18 @@ function AddContractCustomerForm({ match }) {
                       </th>
                       <th
                         style={{
-                          display: 'flex',
-                          justifyContent: 'flex-end',
+                          display: "flex",
+                          justifyContent: "flex-end",
                         }}
                       >
-                        {' '}
-                        <div className='col-sm-8'>
+                        {" "}
+                        <div className="col-sm-8">
                           <input
-                            type='number'
-                            className='form-control form-control-sm contractInput'
-                            id='inputPassword'
-                            placeholder='Chegirma %'
-                            min='0'
+                            type="number"
+                            className="form-control form-control-sm contractInput"
+                            id="inputPassword"
+                            placeholder="Chegirma %"
+                            min="0"
                           />
                         </div>
                         %
@@ -156,14 +156,14 @@ function AddContractCustomerForm({ match }) {
                       <th>
                         <h6>DOWNPAYMENT</h6>
                       </th>
-                      <th style={{ textAlign: 'end' }}>0.00</th>
+                      <th style={{ textAlign: "end" }}>0.00</th>
                     </tr>
                     {/* to'lov qarz */}
                     <tr>
                       <th>
                         <h6>LOAN AMOUNT</h6>
                       </th>
-                      <th style={{ textAlign: 'end' }}>0.00</th>
+                      <th style={{ textAlign: "end" }}>0.00</th>
                     </tr>
                     {/* oy */}
                     <tr>
@@ -171,17 +171,17 @@ function AddContractCustomerForm({ match }) {
                         <h6>MONTHS</h6>
                       </th>
                       <th
-                        style={{ display: 'flex', justifyContent: 'flex-end' }}
+                        style={{ display: "flex", justifyContent: "flex-end" }}
                       >
-                        {' '}
-                        <div className='col-sm-8'>
+                        {" "}
+                        <div className="col-sm-8">
                           <input
-                            type='number'
-                            className='form-control form-control-sm contractInput'
-                            id='inputPassword'
-                            min='1'
-                            max='24'
-                            placeholder='Oyni kiriting'
+                            type="number"
+                            className="form-control form-control-sm contractInput"
+                            id="inputPassword"
+                            min="1"
+                            max="24"
+                            placeholder="Oyni kiriting"
                           />
                         </div>
                         oy
@@ -192,19 +192,19 @@ function AddContractCustomerForm({ match }) {
                       <th>
                         <h6>BANK</h6>
                       </th>
-                      <th style={{ textAlign: 'end' }}>
+                      <th style={{ textAlign: "end" }}>
                         <input
                           required
-                          type='search'
-                          className='form-control form-control-sm contractInput'
-                          id='inputPassword'
-                          list='languages'
-                          name='bank'
-                          id='bank'
+                          type="search"
+                          className="form-control form-control-sm contractInput"
+                          id="inputPassword"
+                          list="languages"
+                          name="bank"
+                          id="bank"
                           value={data.bank}
                           onChange={(e) => inputHandler(e)}
-                          placeholder='Bank Nomini kiriting'
-                          autoComplete='off'
+                          placeholder="Bank Nomini kiriting"
+                          autoComplete="off"
                         />
                         {/* <input
                         required
@@ -219,7 +219,7 @@ function AddContractCustomerForm({ match }) {
                         style={{ marginBottom: "1rem" }}
                         autoComplete="off"
                       /> */}
-                        <datalist id='languages'>
+                        <datalist id="languages">
                           {bankNames.map((province, index) => (
                             <option value={province} key={index} />
                           ))}
@@ -234,16 +234,16 @@ function AddContractCustomerForm({ match }) {
                     <th style={{ textAlign: "end" }}>0.00</th>
                   </tr> */}
                   </thead>
-                </Table>{' '}
+                </Table>{" "}
                 <br />
-                <Col style={{ direction: 'rtl', marginLeft: '0.9rem' }}>
+                <Col style={{ direction: "rtl", marginLeft: "0.9rem" }}>
                   <thead>
                     <tr>
                       <th>
                         <h3>$100</h3>
                       </th>
                       <th>
-                        <h3 style={{ color: '#00ced1' }}>MONTHLY PAYMENT</h3>
+                        <h3 style={{ color: "#00ced1" }}>MONTHLY PAYMENT</h3>
                       </th>
                     </tr>
                   </thead>
@@ -254,7 +254,7 @@ function AddContractCustomerForm({ match }) {
         </Form>
       </div>
     </>
-  )
+  );
 }
 
-export default AddContractCustomerForm
+export default AddContractCustomerForm;
--- frontend/src/screens/CartPage/cart.js
+++ frontend/src/screens/CartPage/cart.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import products from '../../api/products'
+import React from "react";
+import products from "../../api/products";
 
 function getCartFromLocalStorage() {
-  return localStorage.getItem('cart')
-    ? JSON.parse(localStorage.getItem('cart'))
-    : []
+  return localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
 }
 
-const CartContext = React.createContext()
+const CartContext = React.createContext();
 
 function CartProvider({ children, match }) {
-  const [cart, setCart] = React.useState(getCartFromLocalStorage())
+  const [cart, setCart] = React.useState(getCartFromLocalStorage());
   // const [itemTotal, setItemTotal] = React.useState(new Map());
-  const [subTotal, setSubTotal] = React.useState(0)
-  const [loanAmount, setLoanAmount] = React.useState(0)
-  const [discount, setDiscount] = React.useState(0)
-  const [downpayment, setDownpayment] = React.useState(0)
-  const [product, setProduct] = React.useState({})
-  const [cartItems, setCartItems] = React.useState(0)
+  const [subTotal, setSubTotal] = React.useState(0);
+  const [loanAmount, setLoanAmount] = React.useState(0);
+  const [discount, setDiscount] = React.useState(0);
+  const [downpayment, setDownpayment] = React.useState(0);
+  const [product, setProduct] = React.useState({});
+  const [cartItems, setCartItems] = React.useState(0);
 
   React.useEffect(() => {
     // local storage
-    localStorage.setItem('cart', JSON.stringify(cart))
+    localStorage.setItem("cart", JSON.stringify(cart));
     // cart items
     let newCartItems = cart.reduce((total, cartItem) => {
-      return (total += cartItem.amount)
-    }, 0)
-    setCartItems(newCartItems)
+      return (total += cartItem.amount);
+    }, 0);
+    setCartItems(newCartItems);
 
     // subTotal
     let newTotal = cart.reduce((subTotal, cartItem) => {
-      return (subTotal += cartItem.amount * cartItem.price)
-    }, 0)
-    newTotal = parseFloat(newTotal.toFixed(2))
-    setSubTotal(newTotal)
-  }, [cart])
+      return (subTotal += cartItem.amount * cartItem.installmentPrice);
+    }, 0);
+    newTotal = parseFloat(newTotal.toFixed(2));
+    setSubTotal(newTotal);
+  }, [cart]);
 
   // Loan Amount
   // const newLoanAmount = (loanAmount, subTotal, discount, downpayment) => {
@@ -45,62 +45,62 @@ function CartProvider({ children, match }) {
 
   // remove item
   const removeItem = (id) => {
-    let newCart = [...cart].filter((item) => item.id !== id)
-    setCart(newCart)
-  }
+    let newCart = [...cart].filter((item) => item.id !== id);
+    setCart(newCart);
+  };
 
   // increase amount
   const increaseAmount = (id, amount) => {
     if (amount >= products.countInStock) {
-      return "Boshqa mahsulot qo'sha olmaysiz"
+      return "Boshqa mahsulot qo'sha olmaysiz";
     } else {
       const newCart = [...cart].map((item) => {
         return item.id === id
           ? { ...item, amount: item.amount + 1 }
-          : { ...item }
-      })
-      setCart(newCart)
+          : { ...item };
+      });
+      setCart(newCart);
     }
-  }
+  };
 
   // decrease amount
   const decreaseAmount = (id, amount) => {
     if (amount === 1) {
-      removeItem(id)
-      return
+      removeItem(id);
+      return;
     } else {
       const newCart = [...cart].map((item) => {
         return item.id === id
           ? { ...item, amount: item.amount - 1 }
-          : { ...item }
-      })
-      setCart(newCart)
+          : { ...item };
+      });
+      setCart(newCart);
     }
-  }
+  };
 
   //  add to cart
   const addToCart = (product) => {
-    const { id, image, name, price } = product
-    const item = [...cart].find((item) => item.id === id)
+    const { id, image, name, installmentPrice } = product;
+    const item = [...cart].find((item) => item.id === id);
     if (item) {
-      increaseAmount(id)
-      return
+      increaseAmount(id);
+      return;
     } else {
-      const newItem = { id, image, name, price, amount: 1 }
-      const newCart = [...cart, newItem]
-      setCart(newCart)
+      const newItem = { id, image, name, installmentPrice, amount: 1 };
+      const newCart = [...cart, newItem];
+      setCart(newCart);
     }
-  }
+  };
 
   // add item
   const addItem = (product) => {
-    const { id, name, price } = product
-    const item = [...cart]
-  }
+    const { id, name, installmentPrice } = product;
+    const item = [...cart];
+  };
   // clear cart
   const clearCart = () => {
-    setCart([])
-  }
+    setCart([]);
+  };
 
   return (
     <CartContext.Provider
@@ -117,7 +117,7 @@ function CartProvider({ children, match }) {
     >
       {children}
     </CartContext.Provider>
-  )
+  );
 }
 
-export { CartContext, CartProvider }
+export { CartContext, CartProvider };
